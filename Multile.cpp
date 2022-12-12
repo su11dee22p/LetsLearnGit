ffe402ed6a395173e38e52a731554b69dec6e9d6
su11dee22p <khati.sudip111@gmail.com>
2022-12-12 21:38:30 +0545

    Added Multiple.cpp

--- Multile.cpp
+++ Multile.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
+int a,b;
+void Multiply (int x,int y){
+    a=x;
+    b=y;
+
+}
+void display(){
+    cout<<"The Multiple of  A and B is"<<" "<<a*b<<endl;
+}
+
 int main(){
-
-
-
-
+    Multiply(10,20);
+    display();
     return 0;
+
 }
